ad6430518856e60bfa19f550d0d90d02ee0a9537
David-Estevez <david.estevez.fdez@gmail.com>
2013-11-10 18:35:35 +0100

    Added components to SpaceInv.

--- src/SpaceInv.vhd
+++ src/SpaceInv.vhd
@@ -21,10 +21,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity SpaceInv is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           HSync : out  STD_LOGIC;
+			  Test: in STD_LOGIC; 	  
+           Izquierda, Derecha: in STD_LOGIC;
+			  HSync : out  STD_LOGIC;
            VSync : out  STD_LOGIC;
-           R,G,B : out  STD_LOGIC;
-			  Test: in STD_LOGIC);
+           R,G,B : out  STD_LOGIC
+);
 end SpaceInv;
 
 architecture Behavioral of SpaceInv is
@@ -55,14 +57,90 @@ architecture Behavioral of SpaceInv is
 		  );
 	END COMPONENT;
 	
+	-- Component declaration for player spaceship control block:
+	COMPONENT spaceship
+	PORT (  
+			clk : in STD_LOGIC;
+			reset : in  STD_LOGIC;
+         left : in  STD_LOGIC;
+         right : in  STD_LOGIC;
+         enable : in  STD_LOGIC;
+         posH : out  STD_LOGIC_VECTOR (4 downto 0)
+		  );
+	END COMPONENT;
+	
+	-- Component declaration for button edge detector (with/without debouncing )
+	COMPONENT edgeDetector
+	PORT ( 
+			clk: in STD_LOGIC;
+			reset: in STD_LOGIC;
+			enable: in STD_LOGIC;
+			input: in STD_LOGIC;
+			detected: out STD_LOGIC 
+			);
+	END COMPONENT;
+	
 	-- Internal signals
 	signal RGB: STD_LOGIC_VECTOR (2 downto 0);
 	signal X, Y: STD_LOGIC_VECTOR (9 downto 0);
 	
+	-- Output from the edge detectors
+	signal leftDetected: STD_LOGIC;
+	signal rightDetected: STD_LOGIC;
 			
+	-- Dummy signal to be able to compile the code
+	signal dummy: STD_LOGIC;
+	signal dummy_vector: STD_LOGIC_VECTOR( 4 downto 0);
+	
 begin
-   checkerboard: formatoVGA PORT MAP( RGB => RGB, X => X, Y => Y, Test => Test);
-	vgaController: vga PORT MAP( clk => clk, reset => reset, RGB => RGB, HSync => HSync, VSync => VSync, R => R, G => G, B => B, X => X, Y => Y );
-
+   checkerboard: formatoVGA 
+		PORT MAP( 
+					RGB => RGB, 
+					X => X, 
+					Y => Y, 
+					Test => Test
+					);
+					
+	vgaController: vga 
+		PORT MAP( 
+					clk => clk, 
+					reset => reset, 
+					RGB => RGB, 
+					HSync => HSync, 
+					VSync => VSync, 
+					R => R, 
+					G => G, 
+					B => B, 
+					X => X, 
+					Y => Y 
+					);
+					
+	spaceshipControl: spaceship 
+		PORT MAP( 
+					clk => clk,
+					reset => clk,
+					left => leftDetected,
+					right => rightDetected,
+					enable => dummy,
+					posH => dummy_vector 
+					);
+				
+	leftEdgeDetector: edgeDetector
+		PORT MAP(
+					clk => clk,
+					reset => reset,
+					enable => dummy,
+					input => Izquierda,
+					detected => leftDetected
+					);
+					
+	rightEdgeDetector: edgeDetector
+		PORT MAP(
+					clk => clk,
+					reset => reset,
+					enable => dummy,
+					input => Derecha,
+					detected => rightDetected
+					);
 end Behavioral;
 
